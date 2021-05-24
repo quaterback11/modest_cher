bf3dc01a5115b7ec60ae406ea18fdbf58b1cd129
quaterback11 <1quaterback1@gmail.com>
2021-05-24 15:54:42 +0300

    finished

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide(".splide", {
+        autoplay: false,
+        interval: 4000,
+        type: "loop",
+    }).mount();
+});
